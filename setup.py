e23f197833547320bca7ec18beaf53bbcc58cc4c
Fábián Tamás László <tamas.fabian@vengit.com>
2013-02-24 13:41:59 +0100

    Added setup.py, modified TODO

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from distutils.core import setup, Extension
+
+setup(
+    name='pyicap',
+    version='1.0a1',
+    description='A framework for writing ICAP servers',
+    author='FÁBIÁN Tamás László',
+    author_email='giganetom@gmail.com',
+    url='https://github.com/netom/pyicap/',
+    download_url='https://github.com/netom/pyicap/tarball/1.0a1#egg=pyicap-1.0a1',
+    license='BSD License',
+    platforms='OS Independent',
+    py_modules=['pyicap'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ]
+)

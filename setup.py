047eb0f4482a2c8eec0c653365bee37c6f25a0d9
Fábián Tamás László <giganetom@gmail.com>
2017-04-05 21:03:29 +0200

    Version bump in setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 
 setup(
     name='pyicap',
-    version='1.0b',
+    version='1.0b1',
     description='A framework for writing ICAP servers',
     author='FÁBIÁN Tamás László',
     author_email='giganetom@gmail.com',
     url='https://github.com/netom/pyicap/',
-    download_url='https://github.com/netom/pyicap/tarball/1.0a8#egg=pyicap-1.0a8',
+    download_url='https://github.com/netom/pyicap/tarball/1.0b1#egg=pyicap-1.0b1',
     license='BSD License',
     platforms='OS Independent',
     py_modules=['pyicap'],
